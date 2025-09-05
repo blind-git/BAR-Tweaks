85a34f78b4f65afdf1d8bcae4707e8da0054dc7c
Glen Williams <150115252+blind-git@users.noreply.github.com>
2025-09-05 13:37:39 +1000

    Create nojamstealth.lua

---
+++ nojamstealth.lua
@@ -0,0 +1,9 @@
+-- No Jamming/Stealth
+for unitName, uDef in pairs(UnitDefs) do
+    if uDef.radardistancejam and uDef.radardistancejam > 0 then
+        uDef.radardistancejam = 0
+        uDef.stealth = false
+        uDef.onoffable = false
+        uDef.energyupkeep = 0
+    end
+end
