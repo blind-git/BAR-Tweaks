49ad22efee6a838cde2a9f5c9aa8e270834667d9
Glen Williams <150115252+blind-git@users.noreply.github.com>
2025-09-05 13:39:32 +1000

    Create nodragontyrannus.lua

---
+++ nodragontyrannus.lua
@@ -0,0 +1,19 @@
+-- Disable Tyrannus/Dragon
+
+local legaap = UnitDefs["legaap"]
+if legaap and legaap.buildoptions then
+    for i=#legaap.buildoptions,1,-1 do
+        if legaap.buildoptions[i] == "legfort" then
+            table.remove(legaap.buildoptions, i)
+        end
+    end
+end
+
+local coraap = UnitDefs["coraap"]
+if coraap and coraap.buildoptions then
+    for i=#coraap.buildoptions,1,-1 do
+        if coraap.buildoptions[i] == "corcrwh" then
+            table.remove(coraap.buildoptions, i)
+        end
+    end
+end
