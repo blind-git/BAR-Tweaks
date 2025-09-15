1552b45ad13fe38293dedcee649d32f51dc6a0de
Glen Williams <150115252+blind-git@users.noreply.github.com>
2025-09-15 15:28:54 +1000

    Update enable-conturret-buildoptions.lua
    updated to include floating t1 t2 nano platforms and basebuilder turret

--- enable-conturret-buildoptions.lua
+++ enable-conturret-buildoptions.lua
@@ -15,61 +15,74 @@ function deepcopy(a)
 end
 
 local UnitDefs = UnitDefs or {}
-local factions = {'arm', 'cor', 'leg'}
 
--- First part: T1 nano turrets get T1 constructor buildoptions
-for _, faction in ipairs(factions) do 
-    local t1NanoUnits = {faction..'nanotc', faction..'respawn', 'legnanotcbase'}
-    
-    for _, nanoUnit in ipairs(t1NanoUnits) do 
-        if UnitDefs[nanoUnit] then 
-            local t1BuildOptions = deepcopy(UnitDefs[faction..'ck'].buildoptions)
-            UnitDefs[nanoUnit].buildoptions = {}
-            for index, unitName in ipairs(t1BuildOptions) do 
-                UnitDefs[nanoUnit].buildoptions[index] = unitName 
-            end 
-        end 
+-- T1 nano turrets get T1 constructor buildoptions
+local t1Constructors = {
+    armnanotc = 'armck', 
+    armrespawn = 'armck', 
+    cornanotc = 'corck', 
+    correspawn = 'corck', 
+    legnanotc = 'legck', 
+    legrespawn = 'legck', 
+    legnanotcbase = 'legck'
+}
+
+for nanoUnit, constructor in pairs(t1Constructors) do 
+    if UnitDefs[nanoUnit] and UnitDefs[constructor] then 
+        UnitDefs[nanoUnit].buildoptions = deepcopy(UnitDefs[constructor].buildoptions)
     end 
 end
 
--- Second part: T2 nano turrets get extra units
-for _, faction in ipairs(factions) do 
-    -- Units that all factions get
-    local commonT2Units = {
-        faction..'gatet3',   -- Advanced Shield Generator
-        faction..'nanotct2', -- T2 Constructor Turret
-        faction..'wint2',    -- T2 Wind Generator
-        faction..'aap',      -- T2 Aircraft Plant
-        faction..'avp'       -- T2 Vehicle Plant
+-- Floating nano platforms get submarine constructor buildoptions
+local floatingNanos = {
+    -- T1 floating nanos
+    armnanotcplat = 'armcs',
+    cornanotcplat = 'corcs', 
+    legnanotcplat = 'corcs',  -- Legion uses Cortex navy since no Legion navy yet
+    
+    -- T2 floating nanos
+    armnanotc2plat = 'armacsub',
+    cornanotc2plat = 'coracsub',
+    legnanotc2plat = 'coracsub'  -- Legion uses Cortex navy since no Legion navy yet
+}
+
+for floatingNano, subConstructor in pairs(floatingNanos) do 
+    if UnitDefs[floatingNano] and UnitDefs[subConstructor] then 
+        UnitDefs[floatingNano].buildoptions = deepcopy(UnitDefs[subConstructor].buildoptions)
+    end 
+end
+
+-- T2 nano turrets get extra units
+local t2NanoExtras = {
+    armnanotct2 = {
+        constructor = 'armack',
+        commonUnits = {'armgatet3', 'armnanotct2', 'armwint2', 'armaap', 'armavp'},
+        factionUnits = {'armminivulc', 'armbotrail', 'armannit3', 'armshockwave', 'armlwall'}
+    },
+    cornanotct2 = {
+        constructor = 'corack',
+        commonUnits = {'corgatet3', 'cornanotct2', 'corwint2', 'coraap', 'coravp'},
+        factionUnits = {'corminibuzz', 'corhllllt', 'cordoomt3', 'cormwall'}
+    },
+    legnanotct2 = {
+        constructor = 'legack',
+        commonUnits = {'leggatet3', 'legnanotct2', 'legwint2', 'legaap', 'legavp'},
+        factionUnits = {'legmohocon', 'legrwall'}
     }
-    
-    -- Faction-specific extra units
-    local factionSpecificUnits = {}
-    if faction == 'arm' then 
-        factionSpecificUnits = {'armminivulc', 'armbotrail', 'armannit3', 'armshockwave', 'armlwall'}
-    elseif faction == 'cor' then 
-        factionSpecificUnits = {'corminibuzz', 'corhllllt', 'cordoomt3', 'cormwall'}
-    elseif faction == 'leg' then 
-        factionSpecificUnits = {'legmohocon', 'legrwall'}
-    end
-    
-    -- Combine all units to add
-    local allUnitsToAdd = {}
-    for _, unitName in ipairs(commonT2Units) do 
-        table.insert(allUnitsToAdd, unitName)
-    end
-    for _, unitName in ipairs(factionSpecificUnits) do 
-        table.insert(allUnitsToAdd, unitName)
-    end
-    
-    -- Apply to T2 nano turret
-    local t2NanoUnit = faction..'nanotct2'
-    if UnitDefs[t2NanoUnit] then 
+}
+
+for t2NanoUnit, config in pairs(t2NanoExtras) do 
+    if UnitDefs[t2NanoUnit] and UnitDefs[config.constructor] then 
         -- Start with T2 constructor buildoptions as base
-        UnitDefs[t2NanoUnit].buildoptions = deepcopy(UnitDefs[faction..'ack'].buildoptions)
+        UnitDefs[t2NanoUnit].buildoptions = deepcopy(UnitDefs[config.constructor].buildoptions)
         
-        -- Add all the extra units
-        for _, unitName in ipairs(allUnitsToAdd) do 
+        -- Add common units
+        for _, unitName in ipairs(config.commonUnits) do 
+            table.insert(UnitDefs[t2NanoUnit].buildoptions, unitName)
+        end
+        
+        -- Add faction-specific units
+        for _, unitName in ipairs(config.factionUnits) do 
             table.insert(UnitDefs[t2NanoUnit].buildoptions, unitName)
         end 
     end 
