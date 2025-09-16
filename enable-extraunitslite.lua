2bfce965f2568282d5ad26ac907440eb00adedfb
Glen Williams <150115252+blind-git@users.noreply.github.com>
2025-09-16 18:54:03 +1000

    Update enable-extraunitslite.lua

--- enable-extraunitslite.lua
+++ enable-extraunitslite.lua
@@ -1,34 +1,43 @@
--- Extra Units Lite TweakDefs
+-- Extra Units Lite
 local UnitDefs = UnitDefs or {}
 
 -- Table of extra units for each builder
 local extraUnits = {
     -- ARM builders
-    armap = {"armfify"},
-    armvp = {"armzapper"},
-    armshockwave = {}, -- kept empty since we only tweak health/metal elsewhere if needed
-    armshltx = {"armassimilator","armmeatball"},
-    armack = {
-        "armnanotct2", "armshockwave"
+    armap  = {"armfify"},
+    armvp  = {"armzapper"},
+    armshockwave = {},
+    armshltx = {  
+        "armassimilator","armlunchbox","armmeatball","armpwt4","armsptkt4",
+        "armrattet4","armdronecarryland",
     },
+    armack = {"armnanotct2","armshockwave"},
     armaca = {"armnanotct2","armshockwave"},
     armacv = {"armnanotct2"},
-    
-    -- COR builders
+
+    -- ARM T2 navy labs
+    armasy = {"armdecadet3","armpshipt3","coresuppt3","armserpt3","armptt2"},
+
+    -- CORE builders
     coravp = {
         "coracv","corsala","correap","corparrow","corgol","corban","cormart",
         "corvroc","cortrem","corsent","cormabm","coreter","corvrad","cortorch",
         "corgatreap","corftiger","corforge","corsiegebreaker","corvac","corphantom",
-        "cornanotct2"
     },
     corack = {"cornanotct2","cormwall"},
     coraca = {"cornanotct2"},
     coracv = {"cornanotct2"},
+    corgant = {       
+         "corakt4","corkarganetht4","cormandot4","corgolt4","leggobt3","legpede","legsrailt4"
+    },
     corforge = {
         "cortorch","corsala","corftiger","corah","corvrad","coreter","corsolar",
         "corexp","cormakr","coreyes","cordrag","cormaw","corrl","corhlt"
     },
     corvac = {"cornanotc"},
+
+    -- CORE T2 navy labs
+    corasy = {"armdecadet3","armpshipt3","coresuppt3","armserpt3","armptt2"},
 
     -- LEG builders
     legaca = {"legnanotct2","cormwall"},
@@ -36,7 +45,7 @@ local extraUnits = {
     legacv = {"legnanotct2","cormwall"}
 }
 
--- Optional: table for unit-specific health/metalcost tweaks
+-- Unit-specific tweaks
 local unitTweaks = {
     armzapper = {health = 280, speed = 171.0},
     cortorch = {health = 1350}
