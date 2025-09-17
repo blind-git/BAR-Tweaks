d81405d56198225300fb6fdcaa2237c35bd8486d
Glen Williams <150115252+blind-git@users.noreply.github.com>
2025-09-17 22:58:10 +1000

    updated to seperate stealth and jam loops
    fixed as it was only disabling stealth on jammer units previously

--- disable-jammingstealthy.lua
+++ disable-jammingstealthy.lua
@@ -1,9 +1,14 @@
---Disable Jamming/Stealthy
+-- Disable Jamming & Stealth on all units
 for unitName, uDef in pairs(UnitDefs) do
+    -- Kill radar jamming
     if uDef.radardistancejam and uDef.radardistancejam > 0 then
         uDef.radardistancejam = 0
-        uDef.stealth = false
         uDef.onoffable = false
         uDef.energyupkeep = 0
+    end
+
+    -- Kill stealth (applies to ALL stealth units, not just radar jammers)
+    if uDef.stealth then
+        uDef.stealth = false
     end
 end
